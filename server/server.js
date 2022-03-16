5b3b03fa84cde621cad3d8e448b440eff51ce922
Jacob Morse <jacobsmorse@gmail.com>
2022-03-16 12:31:45 -0600

    switched to middleware

--- server/server.js
+++ server/server.js
@@ -6,14 +6,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//MIDDLEWARE
+app.use(express.static(path.join(__dirname, "../public")));
+app.use("/styles", express.static(path.join(__dirname, "../public")));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
-app.get('/styles', function(req,res) {
-    res.sendFile(path.join(__dirname, "../public/styles.css"))
-})
+//ENDPOINTS
+
+// do this if you are storing multiple things locally in the public folder 
+// and it will handle them all
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public"))
+// })
+
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
+
+// app.get('/styles', function(req,res) {
+//     res.sendFile(path.join(__dirname, "../public/styles.css"))
+// })
 
 // we don't designate a port because the cloud service will
 const port = process.env.PORT || 4005;
